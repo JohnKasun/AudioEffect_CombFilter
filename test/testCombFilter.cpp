78a102eb8c282ec552527bbc0092fb3af0c6f83f
John Kasun <johnkasun@live.com>
2022-06-06 19:14:57 -0400

    Add test case outlines

--- test/testCombFilter.cpp
+++ test/testCombFilter.cpp
@@ -1,1 +1,39 @@
+#include "catch.hpp"
+
+#include "CatchUtil.h"
 #include "CombFilterIf.h"
+
+TEST_CASE("Error Checking") {
+	SECTION("FIR") {
+		FAIL();
+	}
+	SECTION("IIR") {
+		FAIL();
+	}
+}
+
+TEST_CASE("Feedforward") {
+	FAIL();
+}
+
+TEST_CASE("Magnitude") {
+	FAIL();
+}
+
+TEST_CASE("Varying blocks") {
+	SECTION("FIR") {
+		FAIL();
+	}
+	SECTION("IIR") {
+		FAIL();
+	}
+}
+
+TEST_CASE("Zero Input") {
+	SECTION("FIR") {
+		FAIL();
+	}
+	SECTION("IIR") {
+		FAIL();
+	}
+}
